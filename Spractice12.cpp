c2f57d67adff1bf843878c3092a8c2587d1720b6
Lee SeungHyun <easter3163@naver.com>
2016-11-18 12:00:43 +0900

    Create Spractice12.cpp

---
+++ Spractice12.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char str[] = "This is a sample string";
+	char key[] = "aeiou";
+	char *pch;
+	printf("Vowels %s",str);
+	pch = strpbrk(str,key);
+	while(pch!=NULL){
+		printf("%c ",*pch);
+		pch = strpbrk(pch+1,key);
+	}
+	return 0;
+}
